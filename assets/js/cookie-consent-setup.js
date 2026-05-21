4023010b45a6eecc63676b18ea8da4a92d18b94e
IUNONE <110372826+IUNONE@users.noreply.github.com>
2026-05-21 14:19:52 +0000

    Deploying to gh-pages from @ IUNONE/zhangshenghong@37359ad86a23b35dd5ae11fbadd7c4aac9080013 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://iunone.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://iunone.github.io/zhangshenghong/#contact">contact us</a>.'
               }
             ]
           }
